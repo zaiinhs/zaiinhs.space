875d3292a9ed4b34c48525cff4942182b1bc9114
zaiinhs <zainalabidinhsc@gmail.com>
2024-01-01 12:28:35 +0700

    fix: change ui footer and responsive small

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,40 @@
 import { Flex, Stack, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <Stack alignItems="center">
-      <Flex>Menu</Flex>
+    <Stack alignItems="center" borderTop="1px gray solid">
+      <Flex marginTop="1rem" gap="4rem">
+        <Link href="#">
+          <Text fontWeight="bold">Source Code</Text>
+        </Link>
+        <Link href="#">
+          <Text fontWeight="bold">Design</Text>
+        </Link>
+        <Link href="#">
+          <Text fontWeight="bold">Docs</Text>
+        </Link>
+        <Link href="#">
+          <Text fontWeight="bold">Resources</Text>
+        </Link>
+      </Flex>
 
-      <Stack>
-        <Text>Reach me out</Text>
-        <Flex justifyContent="center">
-          <FaGithub />
-          <FaLinkedin />
+      <Stack marginBottom="2rem" marginTop="3rem">
+        <Text color="gray" fontWeight="bold">
+          Reach me out
+        </Text>
+        <Flex gap="1rem" fontSize="2rem" justifyContent="center">
+          <a href="#">
+            <FaGithub color="gray" />
+          </a>
+          <a href="#">
+            <FaLinkedin color="gray" />
+          </a>
         </Flex>
       </Stack>
 
-      <Text>&copy;Zainal Abidin {new Date().getFullYear()}</Text>
+      <Text color="gray">&copy;Zainal Abidin {new Date().getFullYear()}</Text>
     </Stack>
   );
 };
